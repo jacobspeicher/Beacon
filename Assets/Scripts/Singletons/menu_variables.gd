f67c4e2cc9ad577635bf0f00dc1bd689f8df3f56
paetramon <speicherjacob@gmail.com>
2016-05-06 20:41:52 -0400

    Added new fonts and made the menu pretty

--- Assets/Scripts/Singletons/menu_variables.gd
+++ Assets/Scripts/Singletons/menu_variables.gd
@@ -2,40 +2,75 @@ extends Node
 
 var sizes = []
 var pos = []
+var end_pos = []
 var button_value = []
 var index = 0
 
 var screen_size = OS.get_video_mode_size()
 
 func _ready():
+
 	sizes.push_back(Vector2(40, 13))
 	sizes.push_back(Vector2(93, 13))
 	sizes.push_back(Vector2(54, 13))
 	sizes.push_back(Vector2(40, 13))
-	
-	pos.push_back(Vector2(screen_size.x / 2 - 203.5, screen_size.y / 2))
-	pos.push_back(Vector2(screen_size.x / 2 - 103.5, screen_size.y / 2))
-	pos.push_back(Vector2(screen_size.x / 2 + 49.5, screen_size.y / 2))
-	pos.push_back(Vector2(screen_size.x / 2 + 163.5, screen_size.y / 2))
-	
+
+	pos.push_back(Vector2(screen_size.x / 2 - 303, screen_size.y / 2 + 17))
+	pos.push_back(Vector2(screen_size.x / 2 - 203, screen_size.y / 2 + 17))
+	pos.push_back(Vector2(screen_size.x / 2 + 15, screen_size.y / 2 + 17))
+	pos.push_back(Vector2(screen_size.x / 2 + 152, screen_size.y / 2 + 17))
+
+	end_pos.push_back(Vector2(screen_size.x / 2 - 233, screen_size.y / 2 + 17))
+	end_pos.push_back(Vector2(screen_size.x / 2 - 15, screen_size.y / 2 + 17))
+	end_pos.push_back(Vector2(screen_size.x / 2 + 127, screen_size.y / 2 + 17))
+	end_pos.push_back(Vector2(screen_size.x / 2 + 210, screen_size.y / 2 + 17))
+
 	button_value.push_back("story")
 	button_value.push_back("instructions")
 	button_value.push_back("credits")
 	button_value.push_back("quit")
-	
+
+	set_process(true)
+
+func _process(delta):
+
+	screen_size = OS.get_video_mode_size()
+
+	sizes.clear()
+	pos.clear()
+	end_pos.clear()
+
+	sizes.push_back(Vector2(40, 13))
+	sizes.push_back(Vector2(93, 13))
+	sizes.push_back(Vector2(54, 13))
+	sizes.push_back(Vector2(40, 13))
+
+	pos.push_back(Vector2(screen_size.x / 2 - 303, screen_size.y / 2 + 17))
+	pos.push_back(Vector2(screen_size.x / 2 - 203, screen_size.y / 2 + 17))
+	pos.push_back(Vector2(screen_size.x / 2 + 15, screen_size.y / 2 + 17))
+	pos.push_back(Vector2(screen_size.x / 2 + 152, screen_size.y / 2 + 17))
+
+	end_pos.push_back(Vector2(screen_size.x / 2 - 233, screen_size.y / 2 + 17))
+	end_pos.push_back(Vector2(screen_size.x / 2 - 15, screen_size.y / 2 + 17))
+	end_pos.push_back(Vector2(screen_size.x / 2 + 127, screen_size.y / 2 + 17))
+	end_pos.push_back(Vector2(screen_size.x / 2 + 210, screen_size.y / 2 + 17))
+
 func get_size():
 	return sizes[index]
-	
-func get_pos():
+
+func get_start_pos():
 	return pos[index]
-	
+
+func get_end_pos():
+	return end_pos[index]
+
 func get_button_value():
 	return button_value[index]
-	
+
 func dec_index():
 	if index > 0:
 		index -= 1
-	
+
 func inc_index():
 	if index < 3:
 		index += 1
--- Assets/Scripts/draw_selector.gd
+++ Assets/Scripts/draw_selector.gd
@@ -2,6 +2,6 @@
 extends Control
 
 func _draw():
-	draw_rect(Rect2(get_node("/root/menu_variables").get_pos(), get_node("/root/menu_variables").get_size()), Color(250, 0, 0))
-
-
+	# change to a line beneath the text
+	#draw_rect(Rect2(get_node("/root/menu_variables").get_start_pos(), get_node("/root/menu_variables").get_size()), Color(255, 253, 0))
+	draw_line(get_node("/root/menu_variables").get_start_pos(), get_node("/root/menu_variables").get_end_pos(), Color(255, 253, 0), 2)
